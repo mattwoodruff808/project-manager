120fda4ac8b75c31d19486b1d45de254eb7d7c08
mattwoodruff808 <mjwoodruff93@gmail.com>
2022-01-06 22:07:23 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashComponent } from './components/dash/dash.component';
+import { UserComponent } from './components/dash/user/user.component';
+import { ProjectsComponent } from './components/dash/projects/projects.component';
+import { IdeasComponent } from './components/dash/ideas/ideas.component';
+import { ProjectComponent } from './components/project/project.component';
+import { ProgressComponent } from './components/project/progress/progress.component';
+import { DescriptionComponent } from './components/project/description/description.component';
+import { MilestonesComponent } from './components/project/milestones/milestones.component';
+import { QuotesComponent } from './components/project/quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    DashComponent,
+    UserComponent,
+    ProjectsComponent,
+    IdeasComponent,
+    ProjectComponent,
+    ProgressComponent,
+    DescriptionComponent,
+    MilestonesComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule
